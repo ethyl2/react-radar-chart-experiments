b3341325427eabdf7f93eec411d06363c2ee672f
Heather Nuffer <zzzuzz@gmail.com>
2020-02-25 14:01:06 -0700

    Initial setup to display radar chart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ 
+import RadarChart from 'react-svg-radar-chart';
+import 'react-svg-radar-chart/build/css/index.css';
+var randomHexColor = require('random-hex-color')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  {
+    data: {
+      bpm: 0.7,
+      duration: 0.8,
+      key: 0.9,
+      genre: 0.67,
+      popularity: 0.8,
+      mood: 0.4
+    },
+    meta: { color: randomHexColor() },
+    title: 'The Hokey Pokey'
+  },
+  {
+    data: {
+      bpm: 0.7,
+      duration: 0.8,
+      key: 0.9,
+      genre: 0.67,
+      popularity: 0.8,
+      mood: 0.4
+    },
+    meta: { color: randomHexColor() },
+    title: 'The Bunny Hop'
+  },
+  {
+    data: {
+      bpm: 0.3,
+      duration: 0.85,
+      key: 0.7,
+      genre: 0.43,
+      popularity: 0.65,
+      mood: 0.6
+    },
+    meta: { color: randomHexColor() },
+    title: 'The Limbo Song'
+  },
+  {
+    data: {
+      bpm: 0.99,
+      duration: 0.2,
+      key: 0.15,
+      genre: 0.5,
+      popularity: 0.55,
+      mood: 0.3
+    },
+    meta: { color: randomHexColor() },
+    title: 'Macarena'
+  },
+];
+
+const captions = {
+  // columns
+  bpm: 'Beats per minute',
+  duration: 'Duration',
+  key: 'Key',
+  genre: 'Genre',
+  popularity: 'Popularity',
+  mood: 'Mood'
+};
+
+const App = () => {
+  return  (
+    <div>
+      <div>
+        <RadarChart
+          captions={captions}
+          data={data}
+          size={750}
+          />
+        </div>
+        <div>
+          {data.map((item, index) => <h2 style={{color: item.meta.color}}>{index+1}. {item.title}</h2>)}
+        </div>
+      </div>
+    );
 }
-
+ 
 export default App;
